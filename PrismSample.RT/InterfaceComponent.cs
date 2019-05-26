d84f9712ad5932d0c89a2bb24aaf2d8eda005ae6
MunChan Park <kaki104@hanmail.net>
2019-05-26 14:04:45 +0900

    Prism으로 시작하는 UWP app Part10

---
+++ PrismSample.RT/InterfaceComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+
+namespace PrismSample.RT
+{
+    [AllowForWeb]
+    public sealed class InterfaceComponent
+    {
+        private string _answer;
+
+        public string GetAnswer()
+        {
+            //return "The answer is 42.";
+            var answer = _answer ?? "null";
+            return $"The answer is {answer}";
+        }
+
+        public void SetAnswer(string answer)
+        {
+            _answer = answer;
+        }
+    }
+}
