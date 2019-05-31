a44cfcc545c34df28115ba1a08d4399500c041c7
MunChan Park <kaki104@hanmail.net>
2019-05-31 17:59:09 +0900

    Prism으로 시작하는 UWP app Part12

---
+++ PrismSample2019.Core/Models/RunScriptEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrismSample2019.Core.Models
+{
+    /// <summary>
+    /// 자바스크립트 실행 이벤트 아규먼트
+    /// </summary>
+    public class RunScriptEventArgs
+    {
+        /// <summary>
+        /// 스크립트 문자열
+        /// </summary>
+        public IList<string> Scripts { get; set; }
+
+    }
+}
--- PrismSample2019/Views/WebViewPage.xaml.cs
+++ PrismSample2019/Views/WebViewPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using PrismSample2019.Services;
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity.Windows;
+using PrismSample2019.Services;
 using PrismSample2019.ViewModels;
 
 using Windows.UI.Xaml.Controls;
@@ -18,7 +20,11 @@ namespace PrismSample2019.Views
             // In this case teh WebViewService isn't a traditional Service but more of a shim to provide to better
             // separation of View and ViewModel and unit testing of a ViewModel that uses the WebViewService since the
             // WebViewService implements the IWebViewService interface that allows for mocking of the service.
-            ViewModel.WebViewService = new WebViewService(webView);
+            //ViewModel.WebViewService = new WebViewService(webView);
+            var service = PrismUnityApplication.Current.Container
+                .Resolve(typeof(IWebViewService), ""
+                    , new ParameterOverride("webViewInstance", webView)) as IWebViewService;
+            ViewModel.WebViewService = service;
         }
     }
 }
