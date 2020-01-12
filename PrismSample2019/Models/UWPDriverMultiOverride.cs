f98bc56b41071957aa4c58b8d61611986737a73a
kaki104 <kaki104@daum.net>
2020-01-12 21:37:19 +0900

    Part14-15

---
+++ PrismSample2019/Models/UWPDriverMultiOverride.cs
@@ -0,0 +1,34 @@
+﻿using PrismSample2019.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace PrismSample2019.Models
+{
+    public class UWPDriverMultiOverride : IDriver
+    {
+        private readonly ICar _car1;
+        private readonly ICarKey _carKey1;
+        private readonly ICar _car2;
+        private readonly ICarKey _carKey2;
+
+        public UWPDriverMultiOverride(ICar car1, ICarKey carKey1, ICar car2, ICarKey carKey2)
+        {
+            _car1 = car1;
+            _carKey1 = carKey1;
+            _car2 = car2;
+            _carKey2 = carKey2;
+        }
+
+        public async Task RunCarAsync()
+        {
+            var message = $"Running {_car1?.GetType().Name} with {_carKey1.GetType().Name} - {_car1?.Run()} Mile";
+            message += $"\n\rRunning {_car2?.GetType().Name} with {_carKey2.GetType().Name} - {_car2?.Run()} Mile";
+            var dialog = new MessageDialog(message);
+            _ = await dialog.ShowAsync();
+        }
+    }
+}
--- PrismSample2019/ViewModels/MainViewModel.cs
+++ PrismSample2019/ViewModels/MainViewModel.cs
@@ -81,8 +81,149 @@ namespace PrismSample2019.ViewModels
                 case "32":
                     Test32();
                     break;
+                case "41":
+                    Test41();
+                    break;
+                case "42":
+                    Test42();
+                    break;
+                case "43":
+                    Test43();
+                    break;
+                case "44":
+                    Test44();
+                    break;
+                case "51":
+                    Test51();
+                    break;
+                case "52":
+                    Test52();
+                    break;
+                case "53":
+                    Test53();
+                    break;
             }
         }
+
+        /// <summary>
+        /// HierarchicalLifetimeManager
+        /// </summary>
+        private async void Test53()
+        {
+            _unityContainer.RegisterType<ICar, Audi>(new HierarchicalLifetimeManager());
+            _unityContainer.RegisterType<IDriver, UWPDriver>();
+
+            var childContainer = _unityContainer.CreateChildContainer();
+
+            var driver1 = _unityContainer.Resolve<IDriver>();
+            await driver1.RunCarAsync();
+
+            var driver2 = _unityContainer.Resolve<IDriver>();
+            await driver2.RunCarAsync();
+
+            var driver3 = childContainer.Resolve<IDriver>();
+            await driver3.RunCarAsync();
+
+            var driver4 = childContainer.Resolve<IDriver>();
+            await driver4.RunCarAsync();
+        }
+
+        /// <summary>
+        /// ContainerControlledLifetimeManager
+        /// </summary>
+        private async void Test52()
+        {
+            _unityContainer.RegisterType<ICar, Audi>(new ContainerControlledLifetimeManager());
+            _unityContainer.RegisterType<IDriver, UWPDriver>();
+
+            var driver1 = _unityContainer.Resolve<IDriver>();
+            await driver1.RunCarAsync();
+
+            var driver2 = _unityContainer.Resolve<IDriver>();
+            await driver2.RunCarAsync();
+        }
+
+        /// <summary>
+        /// TransientLifetimeManager
+        /// </summary>
+        private async void Test51()
+        {
+            _unityContainer.RegisterType<ICar, Audi>(new TransientLifetimeManager());
+            _unityContainer.RegisterType<IDriver, UWPDriver>();
+
+            var driver1 = _unityContainer.Resolve<IDriver>();
+            await driver1.RunCarAsync();
+
+            var driver2 = _unityContainer.Resolve<IDriver>();
+            await driver2.RunCarAsync();
+        }
+
+        /// <summary>
+        /// DependencyOverride
+        /// </summary>
+        private async void Test44()
+        {
+            _unityContainer.RegisterType<ICar, Audi>();
+            _unityContainer.RegisterType<IDriver, UWPDriver>();
+
+            var driver1 = _unityContainer.Resolve<IDriver>();
+            await driver1.RunCarAsync();
+
+            var driver2 = _unityContainer.Resolve<IDriver>(
+                new DependencyOverride(typeof(ICar), new BMW()));
+            await driver2.RunCarAsync();
+        }
+
+        /// <summary>
+        /// PropertyOverride
+        /// </summary>
+        private async void Test43()
+        {
+            _unityContainer.RegisterType<ICar, Audi>();
+            _unityContainer.RegisterType<IDriver, UWPDriverDependencyAttribute>();
+
+            var driver1 = _unityContainer.Resolve<IDriver>();
+            await driver1.RunCarAsync();
+
+            var driver2 = _unityContainer.Resolve<IDriver>(
+                new PropertyOverride("Car", new BMW()));
+            await driver2.RunCarAsync();
+        }
+
+        /// <summary>
+        /// Override Multiple Parameters
+        /// </summary>
+        private async void Test42()
+        {
+            _unityContainer.RegisterType<IDriver, UWPDriverMultiOverride>();
+
+            var p = new ParameterOverrides
+            {
+                { "car1", new Audi() },
+                { "carKey1", new AudiKey() },
+                { "car2", new BMW() },
+                { "carKey2", new BMWKey() }
+            };
+            var driver = _unityContainer.Resolve<IDriver>(p);
+            await driver.RunCarAsync();
+        }
+
+        /// <summary>
+        /// ParameterOverride
+        /// </summary>
+        private async void Test41()
+        {
+            _unityContainer.RegisterType<ICar, Audi>();
+            _unityContainer.RegisterType<IDriver, UWPDriver>();
+
+            var driver = _unityContainer.Resolve<IDriver>();
+            await driver.RunCarAsync();
+
+            var driver2 = _unityContainer.Resolve<IDriver>(
+                new ParameterOverride("car", new BMW()));
+            await driver2.RunCarAsync();
+        }
+
         /// <summary>
         /// Run-time Configuration Method Injection
         /// </summary>
@@ -95,6 +236,7 @@ namespace PrismSample2019.ViewModels
             var driver = _unityContainer.Resolve<IDriver>();
             await driver.RunCarAsync();
         }
+
         /// <summary>
         /// Method Injection
         /// </summary>
@@ -192,10 +334,14 @@ namespace PrismSample2019.ViewModels
             await driver.RunCarAsync();
         }
 
+        /// <summary>
+        /// Register Instance
+        /// </summary>
         private async void Test7()
         {
             _unityContainer.RegisterType<ICar, Audi>();
             var car = _unityContainer.Resolve<ICar>();
+            //register a singleton using Container.RegisterType<IInterface, Type>(new ContainerControlledLifetimeManager());
             _unityContainer.RegisterInstance(car);
 
             var driver1 = _unityContainer.Resolve<UWPDriver>();
@@ -206,6 +352,9 @@ namespace PrismSample2019.ViewModels
             await driver2.RunCarAsync();
         }
 
+        /// <summary>
+        /// Register Driver type
+        /// </summary>
         private async void Test6()
         {
             _unityContainer.RegisterType<ICar, BMW>();
@@ -222,6 +371,9 @@ namespace PrismSample2019.ViewModels
             await driver2.RunCarAsync();
         }
 
+        /// <summary>
+        /// Register Named Type
+        /// </summary>
         private async void Test5()
         {
             _unityContainer.RegisterType<ICar, BMW>();
@@ -237,6 +389,9 @@ namespace PrismSample2019.ViewModels
 
         }
 
+        /// <summary>
+        /// Multiple Registration
+        /// </summary>
         private async void Test4()
         {
             _unityContainer.RegisterType<ICar, BMW>();
@@ -246,6 +401,9 @@ namespace PrismSample2019.ViewModels
             await driver.RunCarAsync();
         }
 
+        /// <summary>
+        /// Resolve 2Driver
+        /// </summary>
         private async void Test3()
         {
             _unityContainer.RegisterType<ICar, BMW>();
